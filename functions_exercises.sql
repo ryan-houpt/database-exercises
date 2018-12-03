c40e0340dff00e0a1b0f90000cfe3f6a29fa5da4
Ryan-Houpt <rehoupt@gmail.com>
2018-12-03 13:47:28 -0600

    functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,24 +2,14 @@ USE employees;
 
 SELECT * FROM Employees;
 
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name DESC, first_name DESC;
-
-SELECT concat('first_name', 'last_name')
+SELECT concat(first_name, " ", last_name)
 FROM employees
 WHERE last_name LIKE ('E%')
 ORDER BY emp_no DESC;
 
-
-SELECT first_name, last_name, hire_date, birth_date
+SELECT DATEDIFF(current_date, hire_date)
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND hire_date LIKE '%-12-25'
-ORDER BY hire_date ASC, birth_date ASC;
-
-
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE first_name LIKE 'd%';
+ORDER BY hire_date ASC, birth_date ASC
+LIMIT 5 OFFSET 100;
