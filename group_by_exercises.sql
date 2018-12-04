f42b4346027e579edbc1a643913396833a07e0f9
Ryan-Houpt <rehoupt@gmail.com>
2018-12-04 12:20:07 -0600

    exercise updates

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -13,6 +13,11 @@ GROUP BY first_name, last_name;
 SELECT last_name, count(*)
 FROM employees
 WHERE last_name like ('%q%')
+   and last_name not like ('%qu%')
+GROUP BY last_name;
 
 
-select concat(emp_no, " ", "-" , " ", last_name, ",", " ", first_name) as full_name, birth_date as DOB from employees limit 10;
+SELECT gender, count(*)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
